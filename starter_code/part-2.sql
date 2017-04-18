aae80533c74d9380ab7b5dbb720c8864c09c808d
celgutierrez <celgutierrez42@gmail.com>
2017-04-17 20:11:03 -0700

    completed parts 1-3

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,14 @@
 -- Insert Data
+INSERT INTO owner(name, age) VALUES ('Donald', 29);
+INSERT INTO owner(name, age) VALUES ('John', 33);
+INSERT INTO owner(name, age) VALUES ('Jane', 43);
+INSERT INTO owner(name, age) VALUES ('Roger', 25);
+INSERT INTO owner(name, age) VALUES ('Erin', 34);
+INSERT INTO owner(name, age) VALUES ('Janet', 30);
+INSERT INTO owner(name, age) VALUES ('Ben',45);
 
+INSERT INTO property(name, units) VALUES('Archstone', 20);
+INSERT INTO property(name, units) VALUES('Willowspring',30);
+INSERT INTO property(name, units) VALUES('Mapletree',25);
+INSERT INTO property(name, units) VALUES('Bridgeway', 35);
+INSERT INTO property(name, units) VALUES('Fairview', 40);
