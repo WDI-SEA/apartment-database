77fef71c175ac85dc34ba1d23034d93fbfffc3eb
alainabuzas <alaina.buzas@gmail.com>
2017-04-17 18:15:42 -0700

    finished-all but num 11- plus two bonus

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,6 @@
 -- Insert Data
+INSERT INTO owner (name, age) VALUES ('Donald', 29), ('John', 33), ('Jane', 43);
+INSERT INTO owner (name, age) VALUES ('Barb', 27), ('Nancy', 39), ('Marco', 36);
 
+INSERT INTO property (name, units, owner_id) VALUES ('Archstone', 20, 2), ('Willowspring', 30, 3);
+INSERT INTO property (name, units, owner_id) VALUES ('Monkeynest', 5, 4), ('Catbox', 9, 5), ('Dogpile', 25, 6);
