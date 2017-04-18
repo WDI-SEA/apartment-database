c44ea4e0bf849a92fa403fae21a734b63914e9cb
sdkevorkian <sdkevorkian@gmail.com>
2017-04-17 18:10:34 -0700

    sql is fun

--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -1,50 +1,51 @@
 -- 1. Show all the data in the owners table.
 
-
+SELECT * FROM owner;
 
 -- 2. Show the names of all owners.
 
-
+SELECT name FROM owner;
 
 -- 3. Show the ages of all of the owners in ascending order.
 
-
+SELECT age FROM owner ORDER BY age ASC;
 
 -- 4. Show the name of an owner whose name is Donald.
 
-
+SELECT name FROM owner WHERE name = 'Donald';
 
 -- 5. Show the age of all owners who are older than 30.
 
-
+SELECT age FROM owner WHERE age > 30;
 
 -- 6. Show the name of all owners whose name starts with an E.
 
-
+SELECT name FROM owner WHERE name LIKE 'E%';
+-- nobody has a name that starts with an E
 
 -- 7. Change Jane's age to 30.
 
-
+UPDATE owner SET age = 30 WHERE name = 'Jane';
 
 -- 8. Change Jane's name to Janet.
 
-
+UPDATE owner SET name = 'Janet' WHERE name = 'Jane';
 
 -- 9. Delete the owner named Janet.
 
-
+DELETE FROM owner WHERE name = 'Janet';
 
 -- 10. Show the names of the first three owners in your owners table.
 
-
+SELECT name FROM owner LIMIT 3;
 
 -- 11. List all properties sorted by the owners names
 
-
+SELECT * FROM property, owner WHERE owner.id = property.owner_id ORDER BY owner.name;
 
 -- 12. Show all of the properties in alphabetical order that are not named Archstone and do not have an id of 3 or 5.
 
-
+SELECT * FROM property WHERE name != 'Archstone' AND id != 3 AND id != 5;
 
 --- Bonuses (attempt at least 2)
 --- These might require you to look up documentation online, or look at the next section in the notes.
@@ -52,16 +53,18 @@
 
 -- 13. In the properties table change the name of the column "name" to "property_name".
 
-
+ALTER TABLE property RENAME COLUMN name TO property_name;
 
 -- 14. Count the total number of properties where the owner_id is between 1 and 3.
 
-
+SELECT COUNT (*) FROM property WHERE owner_id > 0 AND owner_id < 4;
+-- Janet was removed, she would be 3 so 2 is returned
 
 -- 15. Show the highest age of all owners.
 
-
+SELECT age FROM owner ORDER BY age DESC LIMIT 1;
 
 -- 16. Show the name of all owners whose name starts with an E.
 
-
+-- this is number 6
+-- #wherestheduck
