b9f2a84cc1b65265c90cb27bb1ef452d4c32f45a
ttmuangkeo <tim.muangkeo@gmail.com>
2017-04-17 20:39:51 -0700

    added solutions code

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,16 @@
 -- Create tables
 
+CREATE TABLE owner (
+id SERIAL PRIMARY KEY,
+name TEXT,
+age INTEGER
+);
+
+
+
+CREATE TABLE properties (
+	Owner_id  INTEGER,
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	units INTEGER 
+);
