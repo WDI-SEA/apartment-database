276ba7f9c897b63751c4b60d8e6acf6a8a11b96d
justecii <justecii@gmail.com>
2017-09-19 17:23:54 -0700

    sql2 done

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,13 @@
 -- Create tables
+apartments= CREATE TABLE owner (       
+    id SERIAL PRIMARY KEY, 
+    name TEXT,             
+    age INTEGER  
+    );
 
+apartments=# CREATE TABLE property (
+apartments(# name TEXT,
+apartments(# units INTEGER,
+apartments(# owner_id INTEGER,
+apartments(# FOREIGN KEY(owner_id) REFERENCES owner(id)
+apartments(# );
--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,14 @@
 -- Insert Data
+apartments=# INSERT INTO owner (name, age) VALUES ('Donald', 29);
+apartments=# INSERT INTO owner (name, age) VALUES ('John', 33);
+INSERT INTO owner (name, age) VALUES ('Jane', 43);
 
+INSERT INTO owner (name, age) VALUES ('Chad', 37);
+INSERT INTO owner (name, age) VALUES ('AJ', 29);
+INSERT INTO owner (name, age) VALUES ('Liz', 22);
+
+INSERT INTO property (name, units, owner_id) VALUES ('Archstone', 20, 6);
+INSERT INTO property (name, units, owner_id) VALUES ('Willowspring', 20, 4);
+INSERT INTO property (name, units, owner_id) VALUES ('Detroit', 200, 5);
+INSERT INTO property (name, units, owner_id) VALUES ('Toilettown', 8008, 1);
+INSERT INTO property (name, units, owner_id) VALUES ('Hidden Valley', 6, 3);
