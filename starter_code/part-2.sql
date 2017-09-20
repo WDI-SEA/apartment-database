242ca65f5ea59fe82b9539840e4e35d0dd3235c8
stevens1434 <stevens1434@gmail.com>
2017-09-19 17:11:22 -0700

    part two complete

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,16 @@
 -- Insert Data
 
+INSERT INTO owner (name, age) VALUES ('Donald', 29);
+INSERT INTO owner (name, age) VALUES ('John', 33);
+INSERT INTO owner (name, age) VALUES ('Jane', 43);
+INSERT INTO owner (name, age) VALUES ('Peter', 10);
+INSERT INTO owner (name, age) VALUES ('Paul', 10);
+INSERT INTO owner (name, age) VALUES ('Mary', 10);
+INSERT 0 1 --appeared at the end of each "insert into"
+
+INSERT INTO property (name, units) VALUES ('Archstone', 20);
+INSERT INTO property (name, units) VALUES ('Willowspring', 30);
+INSERT INTO property (name, units) VALUES ('Bridgeplace', 10);
+INSERT INTO property (name, units) VALUES ('Cats', 10);INSERT 0 1
+INSERT INTO property (name, units) VALUES ('DeathlyHallows', 21);
+INSERT 0 1 --appeared at the end of each "insert into"
