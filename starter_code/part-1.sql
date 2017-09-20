5f9553c755ceec6fd20b920b5c3641336b20e159
stevens1434 <stevens1434@gmail.com>
2017-09-19 17:10:13 -0700

    part one done

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,20 @@
 -- Create tables
+CREATE TABLE owner(id SERIAL PRIMARY KEY, name TEXT, age INTEGER);
+CREATE TABLE property(id SERIAL PRIMARY KEY, name TEXT, units INTEGER, owner_id INTEGER, FOREIGN KEY (owner_id) REFERENCES owner (id));
 
+
+-- Add things to tables
+INSERT INTO owner (name, age) VALUES ('Donald', 29);
+INSERT INTO owner (name, age) VALUES ('John', 33);
+INSERT INTO owner (name, age) VALUES ('Jane', 43);
+INSERT INTO owner (name, age) VALUES ('Peter', 10);
+INSERT INTO owner (name, age) VALUES ('Paul', 10);
+INSERT INTO owner (name, age) VALUES ('Mary', 10);
+INSERT 0 1 --appeared at the end of each "insert into"
+
+INSERT INTO property (name, units) VALUES ('Archstone', 20);
+INSERT INTO property (name, units) VALUES ('Willowspring', 30);
+INSERT INTO property (name, units) VALUES ('Bridgeplace', 10);
+INSERT INTO property (name, units) VALUES ('Cats', 10);INSERT 0 1
+INSERT INTO property (name, units) VALUES ('DeathlyHallows', 21);
+INSERT 0 1 --appeared at the end of each "insert into"
