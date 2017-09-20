31dcdf7f4f54b862095af295e9041068b89de13e
willylikestokayak <wccoker@gmail.com>
2017-09-19 20:07:13 -0700

    Chad's SQL HW

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,25 @@
 -- Create tables
 
+You are now connected to database "apartments" as user "willylikestokayak".
+apartments=# CREATE TABLE owner(
+apartments(# id SERIAL PRIMARY KEY,
+apartments(# name TEXT,
+apartments(# age INTEGER
+apartments(# );
+CREATE TABLE
+apartments=# CREATE TABLE property(
+apartments(# id SERIAL PRIMARY KEY,
+apartments(# name TEXT,
+apartments(# units INTEGER,
+apartments(# owner_id INTEGER REFERENCES owner(id)
+apartments(# );
+CREATE TABLE
+apartments=# \dt
+               List of relations
+ Schema |   Name   | Type  |       Owner       
+--------+----------+-------+-------------------
+ public | owner    | table | willylikestokayak
+ public | property | table | willylikestokayak
+(2 rows)
+
+
