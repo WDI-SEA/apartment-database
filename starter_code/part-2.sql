974fdb51bdd1f10f32deed482f31c714749744de
hsg2007@yahoo.de <YOUR-EMAIL-ADDRESS>
2017-09-19 22:16:18 -0700

    Completed assignment

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,23 @@
--- Insert Data
+apartments=# SELECT * FROM owners;
+ id |  name  | age 
+----+--------+-----
+  1 | Donald |  29
+  2 | John   |  33
+  3 | Jane   |  43
+  4 | Martha |  35
+  5 | Bob    |  52
+  6 | Abby   |  34
+(6 rows)
+
+
+apartments=# SELECT * FROM properties;
+ id |     name     | units | owners_id 
+----+--------------+-------+-----------
+  1 | Archstone    |    20 |         1
+  2 | Willowspring |    30 |         2
+  3 | Cascadia     |    40 |         3
+  4 | Willowcreek  |    35 |         4
+  5 | Silverfalls  |    25 |         5
+  6 | Gemheights   |    55 |         6
+(6 rows)
 
