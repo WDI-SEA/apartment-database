23dac88ae188306a38cc8d9399bcc3259d489fd8
philpatt <phillip.s.patterson1@gmail.com>
2017-12-19 09:12:34 -0800

    finish deliverable

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,19 @@
 -- Create tables
 
+Phil=# CREATE DATABASE apartments;
+CREATE DATABASE
+Phil=# \connect apartments
+You are now connected to database "apartments" as user "Phil".
+apartments=# CREATE TABLE owner (
+apartments(# id SERIAL PRIMARY KEY,
+apartments(# name TEXT,
+apartments(# age VARCHAR(15));
+CREATE TABLE
+apartments=# CREATE TABLE property (
+apartments(# id SERIAL PRIMARY KEY,
+apartments(# name TEXT,
+apartments(# units VARCHAR(250),
+apartments(# owner_id INT REFERENCES owner(id));
+CREATE TABLE
+
+
