a65be936e0167e5424a5b8831c4756a3e0677c04
Joe Johnson <joe@Joes-MacBook-Pro.local>
2017-12-19 08:56:21 -0800

    Finished.

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,74 @@
 -- Create tables
 
+moviedb=# CREATE DATABASE apartments;
+CREATE DATABASE
+moviedb=# \c apartments
+You are now connected to database "apartments" as user "joe".
+apartments=# CREATE TABLE owner (
+apartments(# id SERIAL PRIMARY KEY,
+apartments(# name TEXT,
+apartments(# age INTEGER
+apartments(# );
+CREATE TABLE
+apartments=# CREATE TABLE property (
+apartments(# id SERIAL PRIMARY KEY,
+apartments(# name TEXT,
+apartments(# units INTEGER,
+apartments(# owner_id INTEGER FOREIGN KEY
+apartments(# );
+ERROR:  syntax error at or near "FOREIGN"
+LINE 5: owner_id INTEGER FOREIGN KEY
+                         ^
+apartments=# CREATE TABLE property (
+id SERIAL PRIMARY KEY,
+name TEXT,
+units INTEGER,
+owner_id FOREIGN KEY
+);
+ERROR:  syntax error at or near "FOREIGN"
+LINE 5: owner_id FOREIGN KEY
+                 ^
+apartments=# CREATE TABLE property (
+id SERIAL PRIMARY KEY,
+name TEXT,
+units INTEGER,
+owner_id int FOREIGN KEY REFERENCES property(id)
+);
+ERROR:  syntax error at or near "FOREIGN"
+LINE 5: owner_id int FOREIGN KEY REFERENCES property(id)
+                     ^
+apartments=# CREATE TABLE property (
+id SERIAL PRIMARY KEY,
+name TEXT,
+units INTEGER,
+owner_id int FOREIGN KEY REFERENCES owner(id)
+);
+ERROR:  syntax error at or near "FOREIGN"
+LINE 5: owner_id int FOREIGN KEY REFERENCES owner(id)
+                     ^
+apartments=# CREATE TABLE property (
+id SERIAL PRIMARY KEY,
+name TEXT,
+units INTEGER,
+FOREIGN KEY (owner_id) REFERENCES owner(id)
+);
+ERROR:  column "owner_id" referenced in foreign key constraint does not exist
+apartments=# CREATE TABLE property (
+id SERIAL PRIMARY KEY,
+name TEXT,
+units INTEGER,
+FOREIGN KEY (owner_id) REFERENCES owner (id)
+);
+ERROR:  column "owner_id" referenced in foreign key constraint does not exist
+apartments=# CREATE TABLE property (
+id SERIAL PRIMARY KEY,
+name TEXT,
+units INTEGER,
+owner_id INTEGER
+);
+CREATE TABLE
+apartments=# FOREIGN KEY (owner_id) REFERENCES owner (id);
+ERROR:  syntax error at or near "FOREIGN"
+LINE 1: FOREIGN KEY (owner_id) REFERENCES owner (id);
+        ^
+apartments=# 
--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -1,67 +1,190 @@
--- 1. Show all the data in the owners table.
+apartments=# SELECT * FROM owner;
+ id |  name   | age
+----+---------+-----
+  1 | Donald  |  29
+  2 | John    |  33
+  3 | Jane    |  43
+  4 | Jack    |  24
+  5 | Jill    |  18
+  6 | Humbert |  50
+(6 rows)
 
+apartments=# SELECT name FROM owner
+apartments-# ;
+  name
+---------
+ Donald
+ John
+ Jane
+ Jack
+ Jill
+ Humbert
+(6 rows)
 
+apartments=# SELECT name, age FROM owner ORDER BY age ASC;
+  name   | age
+---------+-----
+ Jill    |  18
+ Jack    |  24
+ Donald  |  29
+ John    |  33
+ Jane    |  43
+ Humbert |  50
+(6 rows)
 
--- 2. Show the names of all owners.
+apartments=# SELECT name FROM owner WHERE name="Donald";
+ERROR:  column "Donald" does not exist
+LINE 1: SELECT name FROM owner WHERE name="Donald";
+                                          ^
+apartments=# SELECT name="Donald" FROM owner;
+ERROR:  column "Donald" does not exist
+LINE 1: SELECT name="Donald" FROM owner;
+                    ^
+apartments=# SELECT * FROM owner WHERE name = "Donald";
+ERROR:  column "Donald" does not exist
+LINE 1: SELECT * FROM owner WHERE name = "Donald";
+                                         ^
+apartments=# SELECT * FROM owner WHERE name = 'Donald';
+ id |  name  | age
+----+--------+-----
+  1 | Donald |  29
+(1 row)
 
+apartments=# SELECT name, age FROM owner WHERE age > 30;
+  name   | age
+---------+-----
+ John    |  33
+ Jane    |  43
+ Humbert |  50
+(3 rows)
 
+apartments=# SELECT name FROM owner WHERE name = 'E*';
+ name
+------
+(0 rows)
 
--- 3. Show the ages of all of the owners in ascending order.
+apartments=# UPDATE owner SET age=30 WHERE name = 'Jane';
+UPDATE 1
+apartments=# \d+
+                           List of relations
+ Schema |      Name       |   Type   | Owner |    Size    | Description
+--------+-----------------+----------+-------+------------+-------------
+ public | owner           | table    | joe   | 16 kB      |
+ public | owner_id_seq    | sequence | joe   | 8192 bytes |
+ public | property        | table    | joe   | 16 kB      |
+ public | property_id_seq | sequence | joe   | 8192 bytes |
+(4 rows)
 
+apartments=# SELECT * FROM owner;
+ id |  name   | age
+----+---------+-----
+  1 | Donald  |  29
+  2 | John    |  33
+  4 | Jack    |  24
+  5 | Jill    |  18
+  6 | Humbert |  50
+  3 | Jane    |  30
+(6 rows)
 
+apartments=# UPDATE owner SET name = 'Janet' WHERE name = 'Jane';
+UPDATE 1
+apartments=# SELECT * FROM owner;
+ id |  name   | age
+----+---------+-----
+  1 | Donald  |  29
+  2 | John    |  33
+  4 | Jack    |  24
+  5 | Jill    |  18
+  6 | Humbert |  50
+  3 | Janet   |  30
+(6 rows)
 
--- 4. Show the name of an owner whose name is Donald.
+apartments=# DELETE FROM owner WHERE name = 'Janet';
+DELETE 1
+apartments=# SELECT name, age FROM owner ORDER BY id LIMIT 3;
+  name  | age
+--------+-----
+ Donald |  29
+ John   |  33
+ Jack   |  24
+(3 rows)
 
+apartments=# SELECT * FROM property ORDER BY owner_id;
+ id |     name     | units | owner_id
+----+--------------+-------+----------
+  5 | One          |     1 |        1
+  4 | JanePl       |   100 |        3
+  1 | Archstone    |    20 |        4
+  3 | Granada      |    10 |        5
+  2 | Willowspring |    30 |        6
+(5 rows)
 
+apartments=# SELECT * FROM property WHERE name != 'Archstone' && id != 3 || 5;
+ERROR:  syntax error at or near "!="
+LINE 1: ...T * FROM property WHERE name != 'Archstone' && id != 3 || 5;
+                                                             ^
+apartments=# SELECT * FROM property WHERE name != 'Archstone';
+ id |     name     | units | owner_id
+----+--------------+-------+----------
+  2 | Willowspring |    30 |        6
+  3 | Granada      |    10 |        5
+  4 | JanePl       |   100 |        3
+  5 | One          |     1 |        1
+(4 rows)
 
--- 5. Show the age of all owners who are older than 30.
+apartments=# SELECT * FROM property WHERE name != 'Archstone' AND id != 3, 5;
+ERROR:  syntax error at or near ","
+LINE 1: ...CT * FROM property WHERE name != 'Archstone' AND id != 3, 5;
+                                                                   ^
+apartments=# SELECT * FROM property WHERE name != 'Archstone' AND id != 3 AND id != 5;
+ id |     name     | units | owner_id
+----+--------------+-------+----------
+  2 | Willowspring |    30 |        6
+  4 | JanePl       |   100 |        3
+(2 rows)
 
+apartments=# ALTER TABLE property ALTER COLUMN name SET TO property_name;
+ERROR:  syntax error at or near "TO"
+LINE 1: ALTER TABLE property ALTER COLUMN name SET TO property_name;
+                                                   ^
+apartments=# ALTER TABLE property ALTER COLUMN name SET TO 'property_name';
+ERROR:  syntax error at or near "TO"
+LINE 1: ALTER TABLE property ALTER COLUMN name SET TO 'property_name...
+                                                   ^
+apartments=# ALTER TABLE property RENAME COLUMN 'name' TO 'property_name';
+ERROR:  syntax error at or near "'name'"
+LINE 1: ALTER TABLE property RENAME COLUMN 'name' TO 'property_name'...
+                                           ^
+apartments=# ALTER TABLE property RENAME COLUMN name TO property_name;
+ALTER TABLE
+apartments=# SELECT SUM(units) FROM property WHERE id = 1, 2, 3;
+ERROR:  syntax error at or near ","
+LINE 1: SELECT SUM(units) FROM property WHERE id = 1, 2, 3;
+                                                    ^
+apartments=# SELECT SUM(units) FROM property WHERE id = 1-3;
+ sum
+-----
 
+(1 row)
 
--- 6. Show the name of all owners whose name starts with an E.
+apartments=# SELECT SUM(units) FROM property;
+ sum
+-----
+ 161
+(1 row)
 
+apartments=# SELECT age FROM owners ORDER BY age DESC LIMIT 1;
+ERROR:  relation "owners" does not exist
+LINE 1: SELECT age FROM owners ORDER BY age DESC LIMIT 1;
+                        ^
+apartments=# SELECT age FROM owner ORDER BY age DESC LIMIT 1;
+ age
+-----
+  50
+(1 row)
 
-
--- 7. Change Jane's age to 30.
-
-
-
--- 8. Change Jane's name to Janet.
-
-
-
--- 9. Delete the owner named Janet.
-
-
-
--- 10. Show the names of the first three owners in your owners table.
-
-
-
--- 11. List all properties sorted by the owners names
-
-
-
--- 12. Show all of the properties in alphabetical order that are not named Archstone and do not have an id of 3 or 5.
-
-
-
---- Bonuses (attempt at least 2)
---- These might require you to look up documentation online, or look at the next section in the notes.
-
-
--- 13. In the properties table change the name of the column "name" to "property_name".
-
-
-
--- 14. Count the total number of properties where the owner_id is between 1 and 3.
-
-
-
--- 15. Show the highest age of all owners.
-
-
-
--- 16. Show the name of all owners whose name starts with an E.
-
-
+apartments=# SELECT name, age FROM owner ORDER BY age DESC LIMIT 1;
+  name   | age
+---------+-----
+ Humbert |  50
+(1 row)
