27f345aece9e0ce10e0354baac2c2bfc06d1ac29
Scott Molsness <smolsness@gmail.com>
2018-02-14 15:18:51 -0800

    finished deliverable

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,20 @@
+--Place your answers in part-1.sql.
+
+--The owners table should consist of:
+--id (this should be the primary key as well as a unique number that increments automatically)
+--name - name of owner
+--age - age of owner
+--The properties table should consist of:
+--id (this should be the primary key as well as a unique number that increments automatically)
+--name - name of property
+--units - number of units
+--owner_id - reference to owner table
+--Remember to create a foreign key constraint that references the owners table
 -- Create tables
 
+CREATE DATABASE apartments;
+\connect apartments
+CREATE TABLE owner (
+apartments(# id SERIAL PRIMARY KEY, name TEXT, age INTEGER);
+CREATE TABLE property (id SERIAL PRIMARY KEY, name VARCHAR(50), units INTEGER,
+owner_id INTEGER REFERENCES owner(id));
--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,19 @@
--- Insert Data
+--Place your answers in part-2.sql.
 
+--Insert the following owners
+
+--Donald - age 29
+--John - age 33
+--Jane - age 43
+--Add 3 more people (you choose name / age)
+--Insert the following properties (you can pick and choose the property owners)
+
+--Archstone - 20 units
+--Willowspring - 30 units
+--Add 3 more properties (you choose name / units)-- Insert Data
+
+INSERT INTO owner (name, age) VALUES ('Donald', 29), ('John', 33), ('Jane', 43),
+('Nick', 33), ('Jess', 32), ('Winston', 33);
+
+INSERT INTO property (name, units, owner_id) VALUES ('Archstone', 20, 1),
+('Willowspring', 30, 2), ('Brookside', 35, 3), ('Meadowvale', 15, 4), ('Griffin', 20, 5);
