7ba04fcf57b8d43cd4e93db8cd0bcfbec12e4af9
Zakir Butte <zakir.butte@gmailcom>
2018-02-14 15:13:01 -0800

    finished SQL practice

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,10 @@
--- Insert Data
+INSERT INTO owner (name, age)
+VALUES ('Donald', 29),
+('John', 33), ('Jane', 43),
+('Jack', 56), ('Jim', 18), ('Kendrick', 27);
 
+
+INSERT INTO property (name, units)
+VALUES ('Archstone', 20),
+('Willowspring', 30), ('Oakridge', 60),
+('Pinehill', 40), ('Fruitlake', 15), ('Shoehorn', 12);
