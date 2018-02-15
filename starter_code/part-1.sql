2c839206206701b25f21b9db494931f5a30d1208
Chelsea Zhu <qianwei5668@gmail.com>
2018-02-14 16:04:51 -0800

    hw

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,15 @@
--- Create tables
+CREATE TABLE properties (
+id SERIAL,
+name TEXT,
+units INTEGER,
+owner_id INTEGER,
+);
+
+CREATE TABLE owners (
+id SERIAL,
+name TEXT,
+age INTEGER,
+);
+
+
 
--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,10 @@
--- Insert Data
+INSERT INTO owners
+(name, age)
+VALUES
+('Tim', '25'), ('Gen', '30'), ('Henry', '21');
+
+INSERT INTO properties
+(name, units)
+VALUES
+('ARchstone', '20'), ('Willowspring', '30'), ('Juliafred', '24');
 
