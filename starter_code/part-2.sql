42f1852159c76eaacc879c3a3cd7745703e3c381
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-08-13 22:28:53 -0700

    answers

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,24 @@
 -- Insert Data
+INSERT INTO owner (name, age) VALUES ('Donald', 29);
+INSERT INTO owner (name, age) VALUES ('John', 33);
+INSERT INTO owner (name, age) VALUES ('Jane', 43);
+INSERT INTO owner (name, age) VALUES ('Yuki', 67);
+INSERT INTO owner (name, age) VALUES ('Erin', 21);
+INSERT INTO owner (name, age) VALUES ('Siobhan', 55);
+INSERT INTO owner (name, age) VALUES ('Niko', 27);
+INSERT INTO owner (name, age) VALUES ('Alex', 27);
+INSERT INTO owner (name, age) VALUES ('John', 28);
 
+INSERT INTO property (name, units, owner_id) VALUES ('Archstone', 20, 1);
+INSERT INTO property (name, units, owner_id) VALUES ('Zenith Hills', 10, 2);
+INSERT INTO property (name, units, owner_id) VALUES ('Willowspring', 30, 7);
+INSERT INTO property (name, units, owner_id) VALUES ('Ridgefield Bay', 5, 8);
+INSERT INTO property (name, units, owner_id) VALUES ('Brookvista', 20, 9);
+INSERT INTO property (name, units, owner_id) VALUES ('Goldendale', 15, 2);
+INSERT INTO property (name, units, owner_id) VALUES ('Green Haven', 40, 7);
+INSERT INTO property (name, units, owner_id) VALUES ('Fair Creek', 35, 7);
+INSERT INTO property (name, units, owner_id) VALUES ('Parkview Pointe', 50, 7);
+INSERT INTO property (name, units, owner_id) VALUES ('Royal Gardens Court', 45, 7);
+INSERT INTO property (name, units, owner_id) VALUES ('Buena Vista', 100, 7);
+INSERT INTO property (name, units, owner_id) VALUES ('Good Apartments', 500, 7);
+INSERT INTO property (name, units, owner_id) VALUES ('Northwest Court', 1000, 7);
--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -1,9 +1,19 @@
 --- Answers for Questions
 
---- 1.
-
---- 2. 
-
---- etc...
+  1.SELECT * FROM owner
+  2.SELECT name FROM owner;
+  3.SELECT age FROM owner ORDER BY age ASC;
+  4. SELECT * FROM owner WHERE name='Donald';
+  5. SELECT * FROM owner WHERE age>30;
+  6. SELECT * FROM owner WHERE name LIKE 'E%';
+  7. UPDATE owner SET age=30 WHERE name='Jane';
+  8. UPDATE owner SET name='Janet' WHERE name='Jane';
+  9. DELETE FROM owner WHERE name='Janet';
+  10. SELECT name FROM owner WHERE id<5;
+  11. SELECT * FROM owner WHERE name LIKE '%a%';
+  12. SELECT * FROM property WHERE id<>1 AND id<>3 AND id<>5 ORDER BY name;
+  13. SELECT * FROM owner ORDER BY age DESC LIMIT 1;
+  14. SELECT * FROM owner WHERE age<30 AND name LIKE '%o%' LIMIT 1;
+  15. SELECT * FROM property WHERE owner_id<3 AND owner_id>1;
 
 --- Bonuses (if attempted)
