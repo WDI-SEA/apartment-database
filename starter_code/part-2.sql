312f0273ed3385ab86afdc69d569d717ddc6850f
DeusMalsith <metalgear132@gmail.com>
2018-08-13 22:10:50 -0700

    All questions answered but one

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,44 @@
 -- Insert Data
 
+apartments=# INSERT INTO owner (name, age) VALUES('Donald',29);
+
+apartments=# INSERT INTO owner (name, age) VALUES('John', 33);
+
+apartments=# INSERT INTO owner (name, age) VALUES('Jane', 43);
+
+apartments=# INSERT INTO owner (name, age) VALUES('Yuki', 67);
+
+apartments=# INSERT INTO owner (name, age) VALUES('Erin', 21);
+
+apartments=# INSERT INTO owner (name, age) VALUES('Siobhan', 55);
+
+apartments=# INSERT INTO owner (name, age) VALUES('Justin', 29);
+
+apartments=# INSERT INTO owner (name, age) VALUES('Devan', 22);
+
+apartments=# INSERT INTO owner (name, age) VALUES('Brian', 34);
+
+apartments=# INSERT INTO property (name, units, owner_id) VALUES('Zenith Hills', 10, (SELECT id FROM owner WHERE name='Yuki'));
+
+apartments=# INSERT INTO property (name, units, owner_id) VALUES('Willowspring', 30, (SELECT id FROM owner WHERE name='Justin'));
+
+apartments=# INSERT INTO property (name, units, owner_id) VALUES('Ridgefield Bay', 5, (SELECT id FROM owner WHERE name='Erin'));
+
+apartments=# INSERT INTO property (name, units, owner_id) VALUES('Brookvista', 20, (SELECT id FROM owner WHERE name='Devan'));
+
+apartments=# INSERT INTO property (name, units, owner_id) VALUES('Goldenfale', 15, (SELECT id FROM owner WHERE name='Siobhan'));
+
+apartments=# INSERT INTO property (name, units, owner_id) VALUES('Green Haven', 40, (SELECT id FROM owner WHERE name='Jane'));
+
+apartments=# INSERT INTO property (name, units, owner_id) VALUES('Fair Creek', 35, (SELECT id FROM owner WHERE name='Justin'));
+
+apartments=# INSERT INTO property (name, units, owner_id) VALUES('Parkview Pointe', 50, (SELECT id FROM owner WHERE name='Brian'));
+
+apartments=# INSERT INTO property (name, units, owner_id) VALUES('Royal Gardens Court', 45, (SELECT id FROM owner WHERE name='Donald'));
+
+apartments=# INSERT INTO property (name, units, owner_id) VALUES('Millwood Estates', 200, (SELECT id FROM owner WHERE name='Justin'));
+
+apartments=# INSERT INTO property (name, units, owner_id) VALUES('Winterhaven', 75, (SELECT id FROM owner WHERE name='Siobhan'));
+
+apartments=# INSERT INTO property (name, units, owner_id) VALUES('Solaris', 25, (SELECT id FROM owner WHERE name='Justin'));
+
--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -1,9 +1,33 @@
 --- Answers for Questions
 
---- 1.
+1. apartments=# SELECT * FROM owner;
 
---- 2. 
+2. SELECT name FROM owner;
 
---- etc...
+3. SELECT age FROM owner ORDER BY age;
+
+4. SELECT name FROM owner WHERE name='Donald';
+
+5. SELECT name FROM owner WHERE age>30;
+
+6. SELECT name FROM owner WHERE name LIKE 'E%';
+
+7. UPDATE owner SET age=30 WHERE name='Jane';
+
+8. UPDATE owner SET name='Janet' WHERE name='Jane';
+
+9. DELETE FROM property where id=(SELECT id FROM owner WHERE name='Janet');
+
+10. SELECT * FROM owner LIMIT 3;
+
+11. SELECT name FROM owner WHERE name LIKE '%a%';
+
+12.
+
+13. SELECT MAX(age) FROM owner;
+
+14. SELECT MAX(age) FROM owner WHERE age < 30;
+
+15. SELECT COUNT(owner_id) FROM property WHERE owner_id <= 3 AND owner_id >= 1;
 
 --- Bonuses (if attempted)
