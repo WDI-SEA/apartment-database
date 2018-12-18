665adb504ee6bd31a344b0e536ceac1f8a385695
tam-en <tamis@leftelbow.com>
2018-12-18 08:53:56 -0800

    everything done except bonus 17

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,13 @@
 -- Create tables
+CREATE TABLE owner(
+id SERIAL PRIMARY KEY,
+name TEXT,
+age SMALLINT
+);
 
+CREATE TABLE property(
+id SERIAL PRIMARY KEY,
+name TEXT,
+units SMALLINT,
+owner_id INT REFERENCES owner(id)
+);
--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,24 @@
 -- Insert Data
+INSERT INTO owner (name, age) VALUES ('Donald', 29);
+INSERT INTO owner (name, age) VALUES ('John', 33);
+INSERT INTO owner (name, age) VALUES ('Jane', 43);
+INSERT INTO owner (name, age) VALUES ('Yuki', 67);
+INSERT INTO owner (name, age) VALUES ('Erin', 21);
+INSERT INTO owner (name, age) VALUES ('Siobhan', 55);
+INSERT INTO owner (name, age) VALUES ('Sneelock', 57);
+INSERT INTO owner (name, age) VALUES ('Rupert', 35);
+INSERT INTO owner (name, age) VALUES ('HoneyBooBoo', 11);
 
+INSERT INTO property (name, units) VALUES ('Archstone', 20);
+INSERT INTO property (name, units) VALUES ('Zenith Hills', 10);
+INSERT INTO property (name, units) VALUES ('Willowspring', 30);
+INSERT INTO property (name, units) VALUES ('Ridgefield Bay', 5);
+INSERT INTO property (name, units) VALUES ('Brookvista', 20);
+INSERT INTO property (name, units) VALUES ('Goldendale', 15);
+INSERT INTO property (name, units) VALUES ('Green Haven', 40);
+INSERT INTO property (name, units) VALUES ('Fair Creek', 35);
+INSERT INTO property (name, units) VALUES ('Parkview Pointe', 50);
+INSERT INTO property (name, units) VALUES ('Royal Gardens Court', 45);
+INSERT INTO property (name, units) VALUES ('Demonic Acres', 666);
+INSERT INTO property (name, units) VALUES ('Pretense Grove', 7);
+INSERT INTO property (name, units) VALUES ('Wilting Heights', 20);
