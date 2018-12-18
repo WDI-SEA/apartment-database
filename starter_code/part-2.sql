d0bac18f879ab5a496c6eae20b1630150a71abf4
hayeun7777 <kellyhypark@gmail.com>
2018-12-17 19:23:13 -0800

    submitting answers

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,21 @@
 -- Insert Data
 
+-- this is how I input data in each row (for owners table)
+-- public folder contains the table in pgAdmin4
+INSERT INTO public.owners(
+	id, name, age)
+	VALUES (10, 'Rob', 99);
+
+-- and calling the table to make sure all data are put together
+SELECT id, name, age
+	FROM public.owners;
+
+-- this is how I input data in each row (for properties table)
+INSERT INTO public.properties(
+	id, name, units, owner_id)
+	VALUES (13,'random prop3', 11, null);
+
+-- and calling the table to make sure all data are put together
+SELECT id, name, units, owner_id
+	FROM public.properties;-- Insert Data
+
--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -1,9 +1,70 @@
 --- Answers for Questions
-
+-- public folder contains the table in pgAdmin4
 --- 1.
+SELECT id, name, age
+	FROM public.owners;
 
 --- 2. 
+SELECT name FROM public.properties;
 
---- etc...
+--- 3. 
+SELECT * FROM public.owners
+ORDER BY age
+ASC
+
+--- 4. 
+SELECT * FROM public.owners
+WHERE name='Donald'
+
+--- 5. 
+SELECT * FROM public.owners
+WHERE age > 30;
+
+--- 6. 
+SELECT * FROM public.owners
+WHERE name like 'E%' ;
+
+--- 7. 
+UPDATE public.owners
+	SET age=30
+	WHERE name='Jane';
+
+--- 8. 
+UPDATE public.owners
+	SET name = 'Janet'
+	WHERE name='Jane';
+
+--- 9. 
+DELETE FROM public.owners
+	WHERE name='Janet';
+
+--- 10.
+SELECT * FROM public.owners
+limit 3;
+
+--- 11.
+SELECT * FROM public.owners
+WHERE name like '%a%' ;
+
+--- 12.
+SELECT id, name, units, owner_id
+FROM public.properties
+WHERE id<>3 AND id<> 5 AND name<>'Archstone' 
+ORDER BY name ASC;
+
+--- 13.
+SELECT MAX(age)
+FROM public.owners; 
+
+--- 14.
+SELECT MAX(age)
+FROM public.owners
+WHERE age<30 AND name like '%o%' ; 
+
+--- 15.
+SELECT SUM(units)
+FROM public.properties
+WHERE owner_id BETWEEN 1 and 3 ; 
+
 
 --- Bonuses (if attempted)
