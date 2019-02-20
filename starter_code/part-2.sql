5ed67415d8ee852aad3a5b6f3dc838941362397f
Carlo Bruno <carlofbruno@gmail.com>
2019-02-19 21:37:11 -0800

    completed deliverable

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,25 @@
 -- Insert Data
+INSERT INTO owners (name, age) VALUES ('Donald', '29');
+INSERT INTO owners (name, age) VALUES ('John', '33');
+INSERT INTO owners (name, age) VALUES ('Jane', '43');
+INSERT INTO owners (name, age) VALUES ('Yuki', '67');
+INSERT INTO owners (name, age) VALUES ('Jake', '43');
+INSERT INTO owners (name, age) VALUES ('Erin', '21');
+INSERT INTO owners (name, age) VALUES ('Siobhan', '55');
+INSERT INTO owners (name, age) VALUES ('Joe', '33');
+INSERT INTO owners (name, age) VALUES ('Jack', '34');
+INSERT INTO owners (name, age) VALUES ('JJ', '35');
 
+INSERT INTO properties (name, units, owner_id) VALUES ('Archstone', '20', '3');
+INSERT INTO properties (name, units, owner_id) VALUES ('Zenith Hills', '10','5');
+INSERT INTO properties (name, units, owner_id) VALUES ('Willowspring', '30','1');
+INSERT INTO properties (name, units, owner_id) VALUES ('Ridgefield Bay', '5','10');
+INSERT INTO properties (name, units, owner_id) VALUES ('Brookvista', '20','2');
+INSERT INTO properties (name, units, owner_id) VALUES ('Goldendale', '15','7');
+INSERT INTO properties (name, units, owner_id) VALUES ('Green Haven', '40','8');
+INSERT INTO properties (name, units, owner_id) VALUES ('Fair Creek', '35','4');
+INSERT INTO properties (name, units, owner_id) VALUES ('Parkview Pointe', '50','4');
+INSERT INTO properties (name, units, owner_id) VALUES ('Royal Gardens Court', '45','9');
+INSERT INTO properties (name, units, owner_id) VALUES ('The Palace', '10','6');
+INSERT INTO properties (name, units, owner_id) VALUES ('The Garden', '20','10');
+INSERT INTO properties (name, units, owner_id) VALUES ('The Plaza', '30','1');
--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -1,9 +1,58 @@
---- Answers for Questions
-
 --- 1.
+SELECT * FROM owners;
 
 --- 2. 
+SELECT name FROM owners;
 
---- etc...
+--- 3.
+SELECT age FROM owners ORDER BY age ASC;
 
---- Bonuses (if attempted)
+--- 4.
+SELECT name FROM owners WHERE name = 'Donald';
+
+--- 5.
+SELECT age FROM owners WHERE age > '30';
+
+--- 6.
+SELECT name FROM owners WHERE name LIKE 'E%';
+
+--- 7.
+UPDATE owners SET age='30' WHERE name='Jane';
+
+--- 8.
+UPDATE owners SET name='Janet' WHERE name='Jane';
+
+--- 9.
+DELETE FROM owners WHERE name ='Janet';
+-- Will throw error
+-- ERROR:  update or delete on table "owners" violates foreign key constraint "properties_owner_id_fkey" on table "properties"
+-- DETAIL:  Key (id)=(3) is still referenced from table "properties"
+
+--- 10.
+SELECT name FROM owners LIMIT 3;
+
+--- 11.
+SELECT name FROM owners WHERE name LIKE '%a%';
+
+--- 12.
+SELECT name FROM properties WHERE name != 'Archstone' AND id != '3' AND id != '5' ORDER BY name;
+
+--- 13.
+SELECT MAX(age) FROM owners;
+
+--- 14.
+SELECT MAX(age) FROM owners WHERE age < 30 AND name LIKE '%o%' LIMIT 1;
+
+--- 15.
+SELECT COUNT(*) FROM properties WHERE owner_id > 1 AND owner_id < 3;
+
+--- 16.
+SELECT properties.* FROM properties
+JOIN owners ON owners.id = properties.owner_id
+ORDER BY owners.name;
+
+--- 17.
+ALTER TABLE properties RENAME COLUMN name TO property_name;
+
+--- 18.
+INSERT INTO properties (property_name, units, owner_id) VALUES ('Blue Island', '30','3');
