a211f86a36085bf25b2112bd94f7c6ff2a0f9530
greypanda13 <greypanda13@gmail.com>
2019-04-06 14:46:58 -0700

    finalized

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,4 +1,4 @@
 --- Create database & tables
-CREATE DATABASE apartments
+CREATE DATABASE apartments;
 CREATE TABLE owner (id SERIAL PRIMARY KEY, name TEXT, age INT);
-CREATE TABLE properties (id SERIAL PRIMARY KEY, name TEXT, units INT, owner_id INT);
+CREATE TABLE properties (id SERIAL PRIMARY KEY, name TEXT, units INT, owner_id INT references owner(id));
