8b6214cf2c0dc9c895e8e7bf447eb1ee0cf2ca10
JustinRobertKing <kingj4@spu.edu>
2019-04-08 08:42:52 -0700

    completed

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,23 @@
--- Insert Data
+INSERT INTO owners (name, age) VALUES ('Donald', 29);
+INSERT INTO owners (name, age) VALUES ('John', 33);
+INSERT INTO owners (name, age) VALUES ('Jane', 43);
+INSERT INTO owners (name, age) VALUES ('Yuki', 67);
+INSERT INTO owners (name, age) VALUES ('Erin', 21);
+INSERT INTO owners (name, age) VALUES ('Siobhan', 55);
+INSERT INTO owners (name, age) VALUES ('Kevin', 30);
+INSERT INTO owners (name, age) VALUES ('Josie', 26);
+INSERT INTO owners (name, age) VALUES ('Archer', 35);
 
+INSERT INTO properties (name, units, owner_id) VALUES ('Archstone', 20, 7);
+INSERT INTO properties (name, units, owner_id) VALUES ('Zenith Hills', 10, 4);
+INSERT INTO properties (name, units, owner_id) VALUES ('Willowspring', 30, 5);
+INSERT INTO properties (name, units, owner_id) VALUES ('Ridgefield Bay', 5, 1);
+INSERT INTO properties (name, units, owner_id) VALUES ('Brookvista', 20, 5);
+INSERT INTO properties (name, units, owner_id) VALUES ('Goldendale', 15, 3);
+INSERT INTO properties (name, units, owner_id) VALUES ('Green Haven', 40, 2);
+INSERT INTO properties (name, units, owner_id) VALUES ('Fair Creek', 35, 9);
+INSERT INTO properties (name, units, owner_id) VALUES ('Parkview Pointe', 50, 6);
+INSERT INTO properties (name, units, owner_id) VALUES ('Royal Gardens Court', 45, 7);
+INSERT INTO properties (name, units, owner_id) VALUES ('Arbor Heights', 38, 2);
+INSERT INTO properties (name, units, owner_id) VALUES ('River Bluff', 27, 8);
+INSERT INTO properties (name, units, owner_id) VALUES ('Canyon Creek', 20, 9);
--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -1,9 +1,16 @@
---- Answers for Questions
-
---- 1.
-
---- 2. 
-
---- etc...
-
---- Bonuses (if attempted)
+SELECT * FROM owners;
+SELECT name FROM owners;
+SELECT age FROM owners ORDER BY age;
+SELECT name FROM owners WHERE name = 'Donald';
+SELECT age FROM owners WHERE age > 30;
+SELECT name FROM owners WHERE name LIKE 'E%';
+UPDATE owners SET age = 30 WHERE name = 'Jane';
+UPDATE owners SET name = 'Janet' WHERE id = 3;
+DELETE FROM owners WHERE name = 'Janet'; -- Won't let you do this because it's still linked to properties
+SELECT name FROM owners LIMIT 3;
+SELECT name FROM owners WHERE name LIKE '%a%';
+SELECT * FROM properties WHERE name != 'Archstone' AND id != 3 AND id != 5;
+SELECT MAX(age) FROM owners;
+SELECT MAX(age) FROM owners WHERE age < 30 AND name LIKE '%o%';
+SELECT COUNT(name) FROM properties WHERE owner_id <= 3 AND owner_id >= 1;
+-- Bonus:
