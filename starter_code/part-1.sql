1a2aead090484ea519bd54f449b0ac81464a7993
shannonzylstra <shannonzylstra@gmail.com>
2019-12-10 10:25:10 -0800

    modify part-1 and part-2 in class

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,20 +1,9 @@
 -- Create tables
--- CREATE DATABASE apartments;
 
 \connect apartments;
 
 DROP TABLE IF EXISTS owner CASCADE;
 DROP TABLE IF EXISTS property CASCADE;
-
--- - The owners table should consist of:
---   + `id` (this should be the primary key as well as a unique number that increments automatically)
---   + `name` - name of owner
---   + `age` - age of owner
--- - The properties table should consist of:
---   + `id` (this should be the primary key as well as a unique number that increments automatically)
---   + `name` - name of property
---   + `units` - number of units
---   + `owner_id` - reference to owner table
 
 CREATE TABLE owner (
     id SERIAL PRIMARY KEY,
@@ -28,7 +17,3 @@ CREATE TABLE property (
     units integer NOT NULL,
     owner_id integer NOT NULL
 );
-
-SELECT * FROM owner;
-
-\db apartments;
