666cb5d7bba4a7d4b15d4970d8e5c2f47bc3cdb6
snipspin <photonenimpuls@gmail.com>
2020-02-10 18:27:24 -0800

    SQL is fun

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,17 @@
+Create Database "apartments";
+
+\connect apartments
 -- Create tables
 
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(10),
+    age INTEGER
+);
+
+CREATE TABLE properties (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30),
+    units INTEGER,
+    owner_id INTEGER
+);
--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -1,9 +1,42 @@
+\connect apartments;
 --- Answers for Questions
 
 --- 1.
-
+select * from owners;
 --- 2. 
-
+select name from owners;
+-- 3.
+select age from owners order by age asc;
+-- 4.
+select name from owners where name = 'Donald';
+-- 5.
+select age from owners where age > 30;
+-- 6.
+select name from owners where name like 'E%';
+-- 7.
+update owners set age = 30 where name = 'Jane';
+-- 8.
+update owners set name = 'Janet' where name = 'Jane';
+-- 9.
+delete from owners where name = 'Janet';
+-- 10.
+select name from owners order by id limit 3;
+-- 11.
+select name from owners where name like '%a%';
+-- 12.
+select * from properties where properties.name not in ('Archstone') and properties.id not in (3, 5) order by name asc;
+-- 13.
+select max(age) from owners;
+-- 14.
+select max(age) from owners where age < 30 and name like '%o%' limit 1;
+-- 15.
+select count(*) from properties join owners on properties.owner_id = owners.id where owners.id between 1 and 3;
 --- etc...
 
 --- Bonuses (if attempted)
+-- 16.
+select * from properties join owners on properties.owner_id = owners.id order by owners.name;
+-- 17.
+alter table properties rename column name to property_name;
+-- 18.
+insert into properties (name, units, owner_id) values ('Drafting Doors', 10, 3);
