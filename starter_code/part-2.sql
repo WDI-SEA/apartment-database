1ed678786bad04cff1758009c3cfaf21d9ee7eef
smcleod86 <mcleod.scott_86@yahoo.com>
2020-04-07 22:12:43 -0700

    SQL commit 1

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,111 @@
 -- Insert Data
+INSERT INTO owners
+(name, age)
+VALUES
+('Donald', '29');
 
+INSERT INTO owners
+(name, age)
+VALUES
+('John', '33');
+
+INSERT INTO owners
+(name, age)
+VALUES
+('Jane', '43');
+
+INSERT INTO owners
+(name, age)
+VALUES
+('Yuki', '67');
+
+INSERT INTO owners
+(name, age)
+VALUES
+('Erin', '21');
+
+INSERT INTO owners
+(name, age)
+VALUES
+('Siobhan', '55');
+
+INSERT INTO owners
+(name, age)
+VALUES
+('Meredith', '48');
+
+INSERT INTO owners
+(name, age)
+VALUES
+('Laura', '31');
+
+INSERT INTO owners
+(name, age)
+VALUES
+('Stella', '10');
+_____________________________________
+
+INSERT INTO properties
+(name, units, owner_id)
+VALUES
+('Archstone', '20', '1');
+
+INSERT INTO properties
+(name, units, owner_id)
+VALUES
+('Zenith Hills', '10', '2');
+
+INSERT INTO properties
+(name, units, owner_id)
+VALUES
+('Willowspring', '30', '3');
+
+INSERT INTO properties
+(name, units, owner_id)
+VALUES
+('Ridgefield Bay', '5', '1');
+
+INSERT INTO properties
+(name, units, owner_id)
+VALUES
+('Brookvista', '20', '4');
+
+INSERT INTO properties
+(name, units, owner_id)
+VALUES
+('Goldendale', '15', '5');
+
+INSERT INTO properties
+(name, units, owner_id)
+VALUES
+('Green Haven', '40', '6');
+
+INSERT INTO properties
+(name, units, owner_id)
+VALUES
+('Fair Creek', '35', '7');
+
+INSERT INTO properties
+(name, units, owner_id)
+VALUES
+('Parkview Pointe', '50', '8');
+
+INSERT INTO properties
+(name, units, owner_id)
+VALUES
+('Royal Gardens Court', '45', '9');
+
+INSERT INTO properties
+(name, units, owner_id)
+VALUES
+('Belcourt', '25', '6');
+
+INSERT INTO properties
+(name, units, owner_id)
+VALUES
+('Terra Blanca', '90', '1');
+
+INSERT INTO properties
+(name, units, owner_id)
+VALUES
+('Reubens Terrace', '2', '3');
--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -1,9 +1,55 @@
 --- Answers for Questions
 
 --- 1.
+SELECT * FROM owners;
 
 --- 2. 
+SELECT names FROM owners;
 
---- etc...
+--- 3.
+SELECT age FROM owners ORDER BY age ASC;
+
+--- 4.
+SELECT name FROM owners WHERE name = 'Donald';
+
+--- 5. 
+SELECT age FROM owners WHERE age > '30';
+
+--- 6.
+SELECT name FROM owners WHERE name LIKE 'E%';
+
+--- 7.
+UPDATE owners SET age = 30 WHERE name = 'Jane';
+
+--- 8. 
+UPDATE owners SET name = 'Janet' WHERE name = 'Jane';
+
+--- 9.
+DELETE FROM owners WHERE name = 'Janet';
+
+--- 10.
+SELECT name FROM owners WHERE name LIKE '%a%'
+
+--- 11. 
+SELECT name FROM owners WHERE id <= 3;
+
+--- 12.
+SELECT name FROM property EXCEPT SELECT name FROM properties WHERE name = "Archstone" AND id = 3, 5;
+
+--- 13.
+SELECT max(age) FROM owners;
+
+--- 14. 
+SELECT max(age) FROM owners WHERE age <= 30 HAVING name LIKE '%o%' LIMIT 1;
+
+--- 15.
+COUNT(units) FROM owners WHERE id
 
 --- Bonuses (if attempted)
+--- 16.
+SELECT * FROM properties INNER JOIN owners ON property.owner_id = owners.id;
+
+--- 17.
+
+
+--- 18.
