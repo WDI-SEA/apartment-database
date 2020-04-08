56b8057524ba2c64a6820f6af265724675157a6e
smcleod86 <mcleod.scott_86@yahoo.com>
2020-04-08 09:49:44 -0700

    class work through

--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -4,7 +4,7 @@
 SELECT * FROM owners;
 
 --- 2. 
-SELECT names FROM owners;
+SELECT name FROM owners;
 
 --- 3.
 SELECT age FROM owners ORDER BY age ASC;
@@ -19,37 +19,41 @@ SELECT age FROM owners WHERE age > '30';
 SELECT name FROM owners WHERE name LIKE 'E%';
 
 --- 7.
-UPDATE owners SET age = 30 WHERE name = 'Jane';
+UPDATE owners SET age = '30' WHERE name = 'Jane';
 
 --- 8. 
 UPDATE owners SET name = 'Janet' WHERE name = 'Jane';
 
 --- 9.
+DELETE FROM property WHERE owner_id = 3;
 DELETE FROM owners WHERE name = 'Janet';
 
 --- 10.
 SELECT name FROM owners WHERE name LIKE '%a%'
 
 --- 11. 
-SELECT name FROM owners WHERE id <= 3;
+SELECT name FROM owners LIMIT 3;
 
 --- 12.
-SELECT name FROM property EXCEPT SELECT name FROM properties WHERE name = "Archstone" AND id = 3, 5;
+SELECT * FROM property WHERE name NOT IN ('Archstone') AND id NOT IN (3, 5) ORDER BY property ASC;
 
 --- 13.
 SELECT max(age) FROM owners;
 
 --- 14. 
-SELECT max(age) FROM owners WHERE age <= 30 HAVING name LIKE '%o%' LIMIT 1;
+SELECT max(age) FROM owners WHERE age <= '30' HAVING name LIKE '%o%' LIMIT 1;
 
 --- 15.
-COUNT(units) FROM owners WHERE id
+SELECT COUNT (*) owner_id FROM property WHERE owner_id <= 3;
 
 --- Bonuses (if attempted)
 --- 16.
 SELECT * FROM properties INNER JOIN owners ON property.owner_id = owners.id;
+SELECT * FROM owner o JOIN property p ON o.id = p.owner_id ORDER BY o.name ASC;
 
 --- 17.
-
+ALTER TABLE property RENAME COLUMN name TO property_name;
 
 --- 18.
+INSERT INTO owners (id, name, age) VALUES (3, 'Jane', '43');
+INSERT INTO property (name, units, owner_id) VALUES ('Goldendale', 15, 3);
