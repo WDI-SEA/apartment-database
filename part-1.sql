000074a0a8a6388c019159cd0d1b39a4018fd8ad
Eliott R Davidson <fussypuppet@Eliotts-MacBook-Pro.local>
2020-06-18 16:48:57 -0700

    finished part 1

---
+++ part-1.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE apartments;
+\c apartments;
+CREATE TABLE owners (id SERIAL PRIMARY KEY, name TEXT, age INTEGER);
+CREATE TABLE properties (id SERIAL PRIMARY KEY, name TEXT, units INTEGER, owner_id INTEGER REFERENCES owners(id));
