f0d55d23a72785709e2ac9e7b1749b7ec69ab6c7
009kings <009kings@gmail.com>
2020-06-18 16:11:02 -0700

    More clarity, more love

--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -1,9 +1,39 @@
 --- Answers for Questions
 
---- 1.
+-- 1. Show all the data in the owners table.
 
---- 2. 
+-- 2. Show the names of all owners.
 
---- etc...
+-- 3. Show the ages of all of the owners in ascending order.
 
---- Bonuses (if attempted)
+-- 4. Show the name of an owner whose name is Donald.
+
+-- 5. Show the age of all owners who are older than 30.
+
+-- 6. Show the name of all owners whose name starts with an E.
+
+-- 7. Change Jane's age to 30.
+
+-- 8. Change Jane's name to Janet.
+
+-- 9. Delete the owner named Janet.
+
+-- 10. Show the names of the first three owners in your owners table.
+
+-- 11. Show the name of all owners whose name contains an `a`.
+
+-- 12. Show all of the properties in alphabetical order that are not named Archstone and do not have an id of 3 or 5.
+
+-- 13. Show the highest age of all owners.
+
+-- 14. Show the highest age of owners who are under 30 and whose name contains an `o`. Limit to one result.
+
+-- 15. Count the total number of properties where the owner_id is between 1 and 3.
+
+-- BONUSES
+
+-- 16. List all properties sorted by the owners names 
+
+-- 17. In the properties table change the name of the column "name" to "property_name".
+
+-- 18. Add a new property to the owner with an id of 3.
