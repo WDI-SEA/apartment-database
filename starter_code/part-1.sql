db22f94a0ec85301e2bee028ff53be3097d7440f
jdephil <jennifer.dephillips@gmail.com>
2020-06-18 22:40:35 -0400

    1 and 2 done because i forgot the rule

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,15 @@
 -- Create tables
+dephillips=# CREATE DATABASE apartments;
+CREATE DATABASE
+dephillips=# CREATE TABLE owners (
+dephillips(# id SERIAL PRIMARY KEY,
+dephillips(# name TEXT,
+dephillips(# age INTEGER );
+CREATE TABLE
+dephillips=# CREATE TABLE properties (
+dephillips(# id SERIAL PRIMARY KEY,
+dephillips(# name TEXT,
+dephillips(# units INTEGER,
+dephillips(# owner_id INTEGER REFERENCES owners(id)
+dephillips(# );
 
--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -1,27 +1,28 @@
 --- Answers for Questions
 
 -- 1. Show all the data in the owners table.
-
+SELECT * FROM owners;
 -- 2. Show the names of all owners.
-
+SELECT names FROM owner;
 -- 3. Show the ages of all of the owners in ascending order.
-
+SELECT age FROM owners SORT BY age;
 -- 4. Show the name of an owner whose name is Donald.
-
+SELECT name FROM owners WHERE name = 'Donald';
 -- 5. Show the age of all owners who are older than 30.
-
+SELECT age FROM owners WHERE age > 30;
 -- 6. Show the name of all owners whose name starts with an E.
-
+SELECT name FROM owners WHERE name LIKE 'E%';
 -- 7. Change Jane's age to 30.
-
+UPDATE owners SET age=30 WHERE name='Jane';
 -- 8. Change Jane's name to Janet.
-
+UPDATE owners SET name='Janet' WHERE id=3;
 -- 9. Delete the owner named Janet.
-
+UPDATE properties SET owner_id=8 WHERE name='Willowspring';
+DELETE FROM owners WHERE name = 'Janet';
 -- 10. Show the names of the first three owners in your owners table.
-
+SELECT name FROM owners LIMIT 3;
 -- 11. Show the name of all owners whose name contains an `a`.
-
+SELECT name FROM owners WHERE name LIKE '%a%';
 -- 12. Show all of the properties in alphabetical order that are not named Archstone and do not have an id of 3 or 5.
 
 -- 13. Show the highest age of all owners.
