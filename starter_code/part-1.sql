5482672415eb9fc13961006d8e91435be62fea5f
Louis <lreillywillhight@gmail.com>
2020-06-18 17:07:34 -0700

    part 1

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,14 @@
 -- Create tables
 
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  age INT
+);
+
+CREATE TABLE properties (
+  id  SERIAL PRIMARY KEY,
+  name TEXT,
+  units INT,
+  owner_id INT
+);
