f3f1514f9a58cad7f4c3a8bce0a1c88fb36876ae
zbarovsky <zachary.barovsky@gmail.com>
2020-06-18 17:35:33 -0700

    parts one and two are complete. Half of part 3 is done

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,20 @@
 -- Create tables
+zackbarovsky=# CREATE DATABASE apartments;
+CREATE DATABASE
+zackbarovsky=# \c apartments 
+You are now connected to database "apartments" as user "zackbarovsky".
 
+apartments=# CREATE TABLE owners (
+apartments(# id SERIAL PRIMARY KEY,
+apartments(# name TEXT,
+apartments(# age NUMERIC
+apartments(# );
+CREATE TABLE
+apartments=# \d owners
+
+apartments=# CREATE TABLE properties (
+id SERIAL PRIMARY KEY,
+name TEXT,
+units NUMERIC,
+owner_id INT references owners(id) 
+);
--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,24 @@
 -- Insert Data
+INSERT INTO owners (name, age) VALUES ('Donald', 29);
+INSERT INTO owners (name, age) VALUES ('John', 33);
+INSERT INTO owners (name, age) VALUES ('Jane', 43);
+INSERT INTO owners (name, age) VALUES ('Yuki', 67);
+INSERT INTO owners (name, age) VALUES ('Erin', 21);
+INSERT INTO owners (name, age) VALUES ('Siobhan', 55);
+INSERT INTO owners (name, age) VALUES ('Patrick', 69);
+INSERT INTO owners (name, age) VALUES ('Emily', 30);
+INSERT INTO owners (name, age) VALUES ('Stan', 59);
 
+INSERT INTO properties(name, units, owner_id) VALUES ('Archstone', 20, 1);
+INSERT INTO properties(name, units, owner_id) VALUES ('Zenith Hills', 10, 2);
+INSERT INTO properties(name, units, owner_id) VALUES ('Willowspring', 30, 3);
+INSERT INTO properties(name, units, owner_id) VALUES ('Ridgefield Bay' 5, 2);
+INSERT INTO properties(name, units, owner_id) VALUES ('Brookvista', 20, 4);
+INSERT INTO properties(name, units, owner_id) VALUES ('Goldendale', 15, 1);
+INSERT INTO properties(name, units, owner_id) VALUES ('Green Haven', 40, 5);
+INSERT INTO properties(name, units, owner_id) VALUES ('Fair Creek', 35, 5);
+INSERT INTO properties(name, units, owner_id) VALUES ('Parkview Pointe', 50, 6);
+INSERT INTO properties(name, units, owner_id) VALUES ('Royal Gardens Court', 45, 9);
+INSERT INTO properties(name, units, owner_id) VALUES ('Canterbury Court', 70, 9);
+INSERT INTO properties(name, units, owner_id) VALUES ('Madison Park', 100, 8);
+INSERT INTO properties(name, units, owner_id) VALUES ('Kimber Court', 30, 7);
