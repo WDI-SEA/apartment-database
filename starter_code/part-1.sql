b3737934687fb532373180fb55aea3d325310f72
Jameson Aardalen <jamesonaardalen@Jamesons-MBP.attlocal.net>
2020-06-19 09:38:04 -0700

    Inserted data and queried database

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -3,16 +3,16 @@ Create Database "apartments";
 \connect apartments
 
 -- Create tables
-CREATE TABLE "owners" (
-    "id" integer NOT NULL,
-    "name" text NOT NULL,
-    "age" integer NOT NULL,
-    Constraint "owners_id_pkey" PRIMARY KEY ("id")
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    first_name TEXT,
+    age INTEGER NOT NULL,
 );
 
 CREATE TABLE "properties" (
-    "id" integer NOT NULL,
-    "name" text,
-    "units" integer,
-    Constraint "properties_id_pkey" PRIMARY KEY ("id")
+    id INTEGER NOT NULL,
+    property_name VARCHAR(30) NOT NULL,
+    units INTEGER,
+    owner_id BEGIN REFERENCES owners (id),
+    UNIQUE(owner_id)
 );
--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,21 @@
 -- Insert Data
+INSERT INTO owners VALUES ("Donald", 29);
+INSERT INTO owners VALUES ("John", 33);
+INSERT INTO owners VALUES ("Jane", 43);
+INSERT INTO owners VALUES ("Yuki", 67);
+INSERT INTO owners VALUES ("Erin", 21);
+INSERT INTO owners VALUES ("Siobhan", 55);
+INSERT INTO owners VALUES ("George", 24);
+INSERT INTO owners VALUES ("Christian", 25);
 
+INSERT INTO properties (name, units, owner_id) VALUES ("Archstone", 20, 3)
+INSERT INTO properties (name, units, owner_id) VALUES ("Zenith Hills", 10, 1)
+INSERT INTO properties (name, units, owner_id) VALUES ("Willowspring", 30, 1)
+INSERT INTO properties (name, units, owner_id) VALUES ("Ridgefield Bay", 5, 2)
+INSERT INTO properties (name, units, owner_id) VALUES ("Brookvista", 20, 3)
+INSERT INTO properties (name, units, owner_id) VALUES ("Goldendale", 15, 4)
+INSERT INTO properties (name, units, owner_id) VALUES ("Green Haven", 40, 8)
+INSERT INTO properties (name, units, owner_id) VALUES ("Fair Creek", 35, 6)
+INSERT INTO properties (name, units, owner_id) VALUES ("Parkview Pointe", 50, 5)
+INSERT INTO properties (name, units, owner_id) VALUES ("Royal Gardens Court", 45, 6)
+INSERT INTO properties (name, units, owner_id) VALUES ("Archstone", 20, 7)
