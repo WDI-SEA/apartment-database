ddd66b033964b9cb92962d4df527ca6a2bdfde6c
Emily Krauss <e.krauss27@gmail.com>
2020-06-19 09:06:54 -0700

    parts one and two done

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,15 @@
 -- Create tables
 
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  units INTEGER,
+  owners_id INTEGER REFERENCES owners(id)
+);
+
--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,26 @@
 -- Insert Data
 
+INSERT INTO owners (name, age) VALUES ('Donald', 29);
+INSERT INTO owners (name, age) VALUES ('John', 33);
+INSERT INTO owners (name, age) VALUES ('Jane', 43);
+INSERT INTO owners (name, age) VALUES ('Yuki', 67);
+INSERT INTO owners (name, age) VALUES ('Erin', 21);
+INSERT INTO owners (name, age) VALUES ('Siobhan', 55);
+INSERT INTO owners (name, age) VALUES ('Emily', 26);
+INSERT INTO owners (name, age) VALUES ('Stephanie', 36);
+INSERT INTO owners (name, age) VALUES ('Babe', 4);
+
+
+INSERT INTO properties (name, units, owners_id) VALUES ('Archstone', 20, 9);
+INSERT INTO properties (name, units, owners_id) VALUES ('Zenith Hills', 10), 9;
+INSERT INTO properties (name, units, owners_id) VALUES ('Willowspring', 30, 5);
+INSERT INTO properties (name, units, owners_id) VALUES ('Ridgefield Bay', 5, 1);
+INSERT INTO properties (name, units, owners_id) VALUES ('Brookvista', 20, 3);
+INSERT INTO properties (name, units, owners_id) VALUES ('Goldendale', 15, 4);
+INSERT INTO properties (name, units, owners_id) VALUES ('Green Haven', 40, 6);
+INSERT INTO properties (name, units, owners_id) VALUES ('Fair Creek', 35, 2);
+INSERT INTO properties (name, units, owners_id) VALUES ('Parkview Pointe', 50, 8);
+INSERT INTO properties (name, units, owners_id) VALUES ('Royal Gardens Court', 45, 7);
+INSERT INTO properties (name, units, owners_id) VALUES ('Sunset Suite', 13, 7);
+INSERT INTO properties (name, units, owners_id) VALUES ('East Cut Units', 40, 8);
+INSERT INTO properties (name, units, owners_id) VALUES ('Pacheco House', 14, 9);
