f466d22c8306595cc75802f055aaa3ccecc1d826
Nick Foxen <foxen.nicholas@gmail.com>
2020-06-18 19:39:22 -0700

    Part one and part 2 complete

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,13 @@
 -- Create tables
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
 
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  units INTEGER,
+  owner_id INT references owners(id)
+);
--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,66 @@
 -- Insert Data
+INSERT INTO owners (name, age)
+VALUES ('Donald', '29');
 
+INSERT INTO owners (name, age)
+VALUES ('John', '33');
+
+INSERT INTO owners (name, age)
+VALUES ('Jane', '43');
+
+INSERT INTO owners (name, age)
+VALUES ('Yuki', '67');
+
+INSERT INTO owners (name, age)
+VALUES ('Erin', '21');
+
+INSERT INTO owners (name, age)
+VALUES ('Siobhan', '55');
+
+INSERT INTO owners (name, age)
+VALUES ('Nick', '30');
+
+INSERT INTO owners (name, age)
+VALUES ('Jess', '28');
+
+INSERT INTO owners (name, age)
+VALUES ('Diane', '55');
+
+INSERT INTO properties (name , units, owner_id)
+VALUES ('Archstone', '20', 1);
+
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Zenith Hills', '20', 1);
+
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Willowspring', '30', 2);
+
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Ridgefield Bay', '5', 3);
+
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Brookvista', '20', 4);
+
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Goldendale', '15', 4);
+
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Green Haven', '40', 5);
+
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Fair Creek', '35', 6);
+
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Parkview Pointe', '35', 6);
+
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Royal Gardens Court', '45', 7);
+
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Sunnyvale Trailer Park', '30', 7);
+
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Letter Kenny', '10', 8);
+
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Gravity Falls Lake', '35', 9);
