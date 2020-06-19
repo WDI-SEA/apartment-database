0e4f8e805136dcc86779c82e48ea5e0ae0bf8db6
Eryoung-610 <eryoung610@gmail.com>
2020-06-18 18:03:48 -0700

    Small fixes in part 3

--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -25,10 +25,10 @@ WHERE name = "Jane";
 DELETE from owner
 WHERE name = 'Janet';
 
-ERROR:  update or delete on table "owner" violates foreign key constraint "property_owner_id_fkey" on table "property"
-DETAIL:  Key (id)=(3) is still referenced from table "property".
+-- ERROR:  update or delete on table "owner" violates foreign key constraint "property_owner_id_fkey" on table "property"
+-- DETAIL:  Key (id)=(3) is still referenced from table "property".
 
-Cant delete Janet because her id is referenced in property table, therefore if I delete janet it may delete her properties
+-- Cant delete Janet because her id is referenced in property table, therefore if I delete janet it may delete her properties
 
 
 -- Show the names of the first three owners in your owners table.
