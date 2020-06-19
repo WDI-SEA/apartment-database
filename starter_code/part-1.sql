bf7b64f8e970a459cbdd96142863b05d710d4e97
Caitlin Smith <caitlinelizabeth888@gmail.com>
2020-06-19 07:25:56 -0700

    part 1 sql

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,16 @@
 -- Create tables
 
+CREATE DATABASE apartments;
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    name TEXT, 
+    age VARCHAR(3)
+);
+
+CREATE TABLE properties (
+    id SERIAL PRIMARY KEY, 
+    name TEXT, 
+    units INTEGER,
+    owner_id INTEGER,
+    FOREIGN KEY (owner_id) REFERENCES owners(id)
+);
