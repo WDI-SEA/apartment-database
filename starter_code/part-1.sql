98733e80567520fb8515a1ec6b23fd589bcef59d
Neri Ramirez <neriram@Neris-MacBook-Air.local>
2020-06-18 17:01:48 -0700

    part 1 done

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,17 @@
 -- Create tables
+neriram=# CREATE DATABASE apartments;
+CREATE DATABASE
 
+CREATE TABLE owners (
+ id SERIAL PRIMARY KEY,
+ name TEXT,
+ age INTEGER
+);
+CREATE TABLE
+
+CREATE TABLE properties (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    units INTEGER,
+    owner_id INT references owners(id)
+);
