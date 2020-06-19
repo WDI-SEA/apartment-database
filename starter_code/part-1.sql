7ce14c02849250ace227c33caabcf6edd715b450
sky777666 <lmyer12@my.wgu.edu>
2020-06-18 19:33:48 -0700

    Luke Myers apartment HMWK SQL

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,14 @@
 -- Create tables
 
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  name_owner TEXT,
+  age DECIMAL,
+);
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY,
+  name_property TEXT,
+  units DECIMAL,
+  owner_id INTEGER REFERENCES owners(id)
+);
--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,28 @@
 -- Insert Data
 
+INSERT INTO owners (name_owner, age) VALUES ('Donald', 29);
+INSERT INTO owners (name_owner, age) VALUES ('John', 33);
+INSERT INTO owners (name_owner, age) VALUES ('Jane', 43);
+INSERT INTO owners (name_owner, age) VALUES ('Yuki', 67);
+INSERT INTO owners (name_owner, age) VALUES ('Erin', 21);
+INSERT INTO owners (name_owner, age) VALUES ('Siobhan', 55);
+INSERT INTO owners (name_owner, age) VALUES ('Luke', 39);
+INSERT INTO owners (name_owner, age) VALUES ('Amy', 37);
+INSERT INTO owners (name_owner, age) VALUES ('Hunter', 7);
+
+-- insert the following properties (you can pick and choose the property owners)
+
+INSERT INTO properties (name_property, units, owner_id) VALUES ('Archstone',20,1);
+INSERT INTO properties (name_property, units, owner_id) VALUES ('Zenith Hills',10,2);
+INSERT INTO properties (name_property, units, owner_id) VALUES ('Willowspring',30,3);
+INSERT INTO properties (name_property, units, owner_id) VALUES ('Ridgefield Bay ', 20,4);
+INSERT INTO properties (name_property, units, owner_id) VALUES ('Brookvista', 20,5);
+INSERT INTO properties (name_property, units, owner_id) VALUES ('Goldendale',15,6);
+INSERT INTO properties (name_property, units, owner_id) VALUES ('Green Haven',40,7);
+INSERT INTO properties (name_property, units, owner_id) VALUES ('Fair Creek',35,8);
+INSERT INTO properties (name_property, units, owner_id) VALUES ('Parkview Pointe',50,9);
+INSERT INTO properties (name_property, units, owner_id) VALUES ('Royal Gardens Court',45,10);
+INSERT INTO properties (name_property, units, owner_id) VALUES ('Crystal Springs',200, 11);
+INSERT INTO properties (name_property, units, owner_id) VALUES ('Patterson Place',52,12);
+INSERT INTO properties (name_property, units, owner_id) VALUES ('Stoneridge',43,13);
+
