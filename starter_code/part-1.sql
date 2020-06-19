18193a9f67b4071b082302198311b432f5b22f27
Jameson Aardalen <jamesonaardalen@Jamesons-MacBook-Pro.local>
2020-06-19 01:28:22 -0700

    Created database and tables

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,18 @@
--- Create tables
+Create Database "apartments";
 
+\connect apartments
+
+-- Create tables
+CREATE TABLE "owners" (
+    "id" integer NOT NULL,
+    "name" text NOT NULL,
+    "age" integer NOT NULL,
+    Constraint "owners_id_pkey" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "properties" (
+    "id" integer NOT NULL,
+    "name" text,
+    "units" integer,
+    Constraint "properties_id_pkey" PRIMARY KEY ("id")
+);
