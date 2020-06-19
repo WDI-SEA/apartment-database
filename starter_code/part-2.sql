504042bfae716a5c6cf4931a0b4579b87e10fe4f
nguyendm77 <nguyendm77@gmail.com>
2020-06-19 09:02:43 -0700

    submit hw

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,24 @@
 -- Insert Data
+INSERT INTO owners (name, age) VALUES ('Donald', 23);
+INSERT INTO owners (name, age) VALUES ('John', 33);
+INSERT INTO owners (name, age) VALUES ('Jane', 43);
+INSERT INTO owners (name, age) VALUES ('Yuki', 67);
+INSERT INTO owners (name, age) VALUES ('Erin', 21);
+INSERT INTO owners (name, age) VALUES ('SioBhan', 55);
+INSERT INTO owners (name, age) VALUES ('William', 35);
+INSERT INTO owners (name, age) VALUES ('Jonathan', 45);
+INSERT INTO owners (name, age) VALUES ('Charles', 25);
 
+INSERT INTO properties (name, units, owner_id) VALUES ('Archstone', 20, 1);
+INSERT INTO properties (name, units, owner_id) VALUES ('Zenith Hills', 10, 1);
+INSERT INTO properties (name, units, owner_id) VALUES ('Willowspring', 30, 2);
+INSERT INTO properties (name, units, owner_id) VALUES ('Ridgefield Bay', 5, 3);
+INSERT INTO properties (name, units, owner_id) VALUES ('Brookvista', 20, 3);
+INSERT INTO properties (name, units, owner_id) VALUES ('Goldendale', 15, 3);
+INSERT INTO properties (name, units, owner_id) VALUES ('Green Haven', 40, 4);
+INSERT INTO properties (name, units, owner_id) VALUES ('Fair Creek', 35, 4);
+INSERT INTO properties (name, units, owner_id) VALUES ('Parkview Pointe', 50, 5);
+INSERT INTO properties (name, units, owner_id) VALUES ('Royal Gardens Court', 45, 6);
+INSERT INTO properties (name, units, owner_id) VALUES ('Par Terra', 140, 6);
+INSERT INTO properties (name, units, owner_id) VALUES ('Newport Hill', 85, 6);
+INSERT INTO properties (name, units, owner_id) VALUES ('Fairwood', 120, 7);
