74da3560d05f516ec9d3ff80b3e88908454adf40
Nick Hvattum <nhvattum@gmail.com>
2020-06-19 08:59:02 -0700

    Homework completed

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,86 @@
 -- Insert Data
+INSERT into owners
+(name, age)
+VALUES 
+('John', 33);
+INSERT into owners
+(name, age)
+VALUES 
+('Jane', 43);
+INSERT into owners
+(name, age)
+VALUES 
+('Yuki', 67);
+INSERT into owners
+(name, age)
+VALUES 
+('Erin', 21);
+INSERT into owners
+(name, age)
+VALUES 
+('Siobhan', 55);
+INSERT into owners
+(name, age)
+VALUES 
+('Kris', 35);
+INSERT into owners
+(name, age)
+VALUES 
+('Erica', 33);
+INSERT into owners
+(name, age)
+VALUES 
+('Nick', 30);
 
+
+INSERT into properties
+(name, units, owner_id)
+VALUES
+('Archstone', 20, 9);
+INSERT into properties
+(name, units, owner_id)
+VALUES
+('Zenith Hills', 10, 9);
+INSERT into properties
+(name, units, owner_id)
+VALUES
+('Willowspring', 30, 9);
+INSERT into properties
+(name, units, owner_id)
+VALUES
+('Ridgefield Bay', 5, 9);
+INSERT into properties
+(name, units, owner_id)
+VALUES
+('Brookvista', 5, 9);
+INSERT into properties
+(name, units, owner_id)
+VALUES
+('Goldendale', 15, 9);
+INSERT into properties
+(name, units, owner_id)
+VALUES
+('Green Haven', 40, 9);
+INSERT into properties
+(name, units, owner_id)
+VALUES
+('Fair Creek', 35, 9);
+INSERT into properties
+(name, units, owner_id)
+VALUES
+('Parkview Pointe', 50, 9);
+INSERT into properties
+(name, units, owner_id)
+VALUES
+('Royal Gardens Court', 45);
+INSERT into properties
+(name, units, owner_id)
+VALUES('Hvattum Manor', 16, 9);
+INSERT into properties
+(name, units, owner_id)
+VALUES
+('Whitfield Lane', 20, 9);
+INSERT into properties
+(name, units, owner_id)
+VALUES
+('Dashing Dutchman', 80, 9);
--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -1,38 +1,55 @@
 --- Answers for Questions
 
 -- 1. Show all the data in the owners table.
+SELECT * FROM owners;
 
 -- 2. Show the names of all owners.
+SELECT name FROM owners;
 
 -- 3. Show the ages of all of the owners in ascending order.
+SELECT age FROM owners ORDER BY age;
 
 -- 4. Show the name of an owner whose name is Donald.
+SELECT * FROM owners WHERE name = 'Donald';
 
 -- 5. Show the age of all owners who are older than 30.
+SELECT age FROM owners WHERE age > 30;
 
 -- 6. Show the name of all owners whose name starts with an E.
+SELECT name FROM owners WHERE name LIKE 'E%';
 
 -- 7. Change Jane's age to 30.
+UPDATE owners SET age='30' WHERE name='Jane';
 
 -- 8. Change Jane's name to Janet.
+UPDATE owners SET name='Janet' WHERE name='Jane';
 
 -- 9. Delete the owner named Janet.
+DELETE FROM owners WHERE name='Janet';
 
 -- 10. Show the names of the first three owners in your owners table.
+SELECT name FROM owners LIMIT 3;
 
 -- 11. Show the name of all owners whose name contains an `a`.
+SELECT name FROM owners WHERE name LIKE '%a%';
 
 -- 12. Show all of the properties in alphabetical order that are not named Archstone and do not have an id of 3 or 5.
+SELECT * FROM properties WHERE NOT name = 'Archstone' AND NOT id = 3 AND NOT id = 5 ORDER BY name;
 
 -- 13. Show the highest age of all owners.
+SELECT age FROM owners ORDER BY age DESC LIMit 1;
 
 -- 14. Show the highest age of owners who are under 30 and whose name contains an `o`. Limit to one result.
+SELECT age FROM owners WHERE age < 30 AND name LIKE '%o%' ORDER BY age LIMIT 1;
 
 -- 15. Count the total number of properties where the owner_id is between 1 and 3.
+SELECT COUNT(owner_id) FROM properties WHERE owner_id > 0 AND owner_id < 4;
+
 
 -- BONUSES
 
 -- 16. List all properties sorted by the owners names 
+
 
 -- 17. In the properties table change the name of the column "name" to "property_name".
 
