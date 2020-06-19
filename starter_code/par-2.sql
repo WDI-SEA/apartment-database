4abb748066b8edb39b6034370a14254c86527168
Louis <lreillywillhight@gmail.com>
2020-06-18 17:34:21 -0700

    part 2

---
+++ starter_code/par-2.sql
@@ -0,0 +1,28 @@
+-- Part 2
+
+apartments=> INSERT INTO properties (name, units, owner_id) VALUES ('Archstone', 20, 3);
+INSERT 0 1
+apartments=> INSERT INTO properties (name, units, owner_id) VALUES ('Zenith Hills', 10, 4);
+INSERT 0 1
+apartments=> INSERT INTO properties (name, units, owner_id) VALUES ('Willowspring', 30, 1);
+INSERT 0 1
+apartments=> INSERT INTO properties (name, units, owner_id) VALUES ('Ridgefield Bay', 5, 5);
+INSERT 0 1
+apartments=> INSERT INTO properties (name, units, owner_id) VALUES ('Brookvista', 20, 2);
+INSERT 0 1
+apartments=> INSERT INTO properties (name, units, owner_id) VALUES ('Goldendale', 15, 9);
+INSERT 0 1
+apartments=> INSERT INTO properties (name, units, owner_id) VALUES ('Green Haven', 40, 8);
+INSERT 0 1
+apartments=> INSERT INTO properties (name, units, owner_id) VALUES ('Fair Creek', 35, 6);
+INSERT 0 1
+apartments=> INSERT INTO properties (name, units, owner_id) VALUES ('Parkview Pointe', 50, 3);
+INSERT 0 1
+apartments=> INSERT INTO properties (name, units, owner_id) VALUES ('Royal Gardens Court', 45, 7);
+INSERT 0 1
+apartments=> INSERT INTO properties (name, units, owner_id) VALUES ('Dracula Manor', 12, 4);
+INSERT 0 1
+apartments=> INSERT INTO properties (name, units, owner_id) VALUES ('Animal House', 20, 5);
+INSERT 0 1
+apartments=> INSERT INTO properties (name, units, owner_id) VALUES ('Boss Town', 34, 6);
+INSERT 0 1
