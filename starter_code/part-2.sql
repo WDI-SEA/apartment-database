c941f1f6c982c6ee0a53fe8e886d4b5d1697f1d9
d4vves <d4vves@gmail.com>
2020-06-18 20:48:24 -0500

    forgive me senpai, i didn't commit

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,23 @@
 -- Insert Data
-
+INSERT INTO owners (name, age) VALUES ('Donald', 29);
+INSERT INTO owners (name, age) VALUES ('John', 33);
+INSERT INTO owners (name, age) VALUES ('Jane', 43);
+INSERT INTO owners (name, age) VALUES ('Yuki', 67);
+INSERT INTO owners (name, age) VALUES ('Erin', 21);
+INSERT INTO owners (name, age) VALUES ('Siobhan', 55);
+INSERT INTO owners (name, age) VALUES ('Dave', 32);
+INSERT INTO owners (name, age) VALUES ('Aang', 12);
+INSERT INTO owners (name, age) VALUES ('Ed', 18);
+INSERT INTO properties (name, units, owner_id) VALUES ('Archstone', 20, 7);
+INSERT INTO properties (name, units, owner_id) VALUES ('Zenith Hills', 10, 8);
+INSERT INTO properties (name, units, owner_id) VALUES ('Willowspring', 30, 9);
+INSERT INTO properties (name, units, owner_id) VALUES ('Ridgefield Bay', 5, 1);
+INSERT INTO properties (name, units, owner_id) VALUES ('Brookvista', 20, 2);
+INSERT INTO properties (name, units, owner_id) VALUES ('Goldendale', 15, 3);
+INSERT INTO properties (name, units, owner_id) VALUES ('Green Haven', 40, 4);
+INSERT INTO properties (name, units, owner_id) VALUES ('Fair Creek', 35, 5);
+INSERT INTO properties (name, units, owner_id) VALUES ('Parkview Pointe', 50, 6);
+INSERT INTO properties (name, units, owner_id) VALUES ('Royal Gardens Court', 45, 4);
+INSERT INTO properties (name, units, owner_id) VALUES ('Humboldt Park Nice Place', 15, 7);
+INSERT INTO properties (name, units, owner_id) VALUES ('Eastern Air Temple', 60, 8);
+INSERT INTO properties (name, units, owner_id) VALUES ('Resembool', 5, 9);
