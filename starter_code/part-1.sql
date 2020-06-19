782b0ed4062c354d2533b741244a3d607a5e5470
Noa Kim <noanonoa@gmail.com>
2020-06-19 00:23:57 -0700

    answers submitted

--- starter_code/part-1.sql
+++ starter_code/part-1.sql
@@ -1,2 +1,15 @@
 -- Create tables
+create database apartments;
 
+create table owners (
+id serial primary key,
+name text,
+age integer
+);
+
+create table properties (
+id serial primary key,
+name text,
+units integer,
+owner_id integer references owners(id)
+);
--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,45 @@
 -- Insert Data
 
+insert into owners (name, age)
+values ('Donald', 29);
+insert into owners (name, age)
+values ('John', 33);
+insert into owners (name, age)
+values ('Jane', 43);
+insert into owners (name, age)
+values ('Yuki', 67);
+insert into owners (name, age)
+values ('Erin', 21);
+insert into owners (name, age)
+values ('Siobhan', 55);
+insert into owners (name, age)
+values ('Sean', 22);
+insert into owners (name, age)
+values ('Noa', 32);
+insert into owners (name, age)
+values ('Francesca', 31);
+
+insert into properties (name, units, owner_id)
+values ('Archstone', 20, 1);
+insert into properties (name, units, owner_id)
+values ('Zenith Hills', 10, 2);
+insert into properties (name, units, owner_id)
+values ('Willowspring', 30, 3);
+insert into properties (name, units, owner_id)
+values ('Ridgefield Bay', 30, 4);
+insert into properties (name, units, owner_id)
+values ('Brookevista', 20, 5);
+insert into properties (name, units, owner_id)
+values ('Goldendale', 15, 6);
+insert into properties (name, units, owner_id)
+values ('Green Haven', 40, 7);
+insert into properties (name, units, owner_id)
+values ('Fair Creek', 35, 8);
+insert into properties (name, units, owner_id)
+values ('Parkview Pointe', 50, 9);
+insert into properties (name, units, owner_id)
+values ('Royal Gardens Court', 45, 7);
+insert into properties (name, units, owner_id)
+values ('Ridgeview Lane', 30, 8);
+insert into properties (name, units, owner_id)
+values ('Shadow Park', 100, 9);
