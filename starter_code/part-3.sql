ab6223a61a995b6acc4b007246f4c1689f04bb1c
Louis <lreillywillhight@gmail.com>
2020-06-19 09:23:18 -0700

    part 3

--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -1,33 +1,33 @@
 --- Answers for Questions
 
 -- 1. Show all the data in the owners table.
-
+SELECT * FROM owners;
 -- 2. Show the names of all owners.
-
+SELECT name FROM owwners;
 -- 3. Show the ages of all of the owners in ascending order.
-
+SELECT age FROM owners ORDER BY age;
 -- 4. Show the name of an owner whose name is Donald.
-
+SELECT name FROM owners WHERE name = 'Donald';
 -- 5. Show the age of all owners who are older than 30.
-
+SELECT age FROM owners WHERE age > 39
 -- 6. Show the name of all owners whose name starts with an E.
-
+SELECT name FROM owners WHERE name LIKE 'E%';
 -- 7. Change Jane's age to 30.
-
+UPDATE owners SET age = 30 WHERE name = 'Jane';
 -- 8. Change Jane's name to Janet.
-
+UPDATE owners SET name = 'Janet' WHERE name = 'Jane';
 -- 9. Delete the owner named Janet.
-
+DELETE FROM owners WHERE name = 'Janet';
 -- 10. Show the names of the first three owners in your owners table.
-
+SELECT name FROM owners LIMIT 3;
 -- 11. Show the name of all owners whose name contains an `a`.
-
+SELECT name FROM owners WHERE name LIKE '%a%';
 -- 12. Show all of the properties in alphabetical order that are not named Archstone and do not have an id of 3 or 5.
-
+SELECT * FROM properties WHERE name <> 'Archstone' AND id <> 3 AND id <> 5;
 -- 13. Show the highest age of all owners.
-
+SELECT MAX(age) FROM owners;
 -- 14. Show the highest age of owners who are under 30 and whose name contains an `o`. Limit to one result.
-
+SELECT MAX(age) FROM owners WHERE age < 30 AND name LIKE '%o%' LIMIT 1
 -- 15. Count the total number of properties where the owner_id is between 1 and 3.
 
 -- BONUSES
