c15407fd53b7507594a3afc2c65c790432c2965f
Neri Ramirez <neriram@Neris-MacBook-Air.local>
2020-06-18 17:18:28 -0700

    part 2 done

--- starter_code/part-2.sql
+++ starter_code/part-2.sql
@@ -1,2 +1,46 @@
 -- Insert Data
+INSERT INTO owners (name, age)
+VALUES ('Donald', '29');
+INSERT INTO owners (name, age)
+VALUES ('John', '33');
+INSERT INTO owners (name, age)
+VALUES ('Jane', '43');
+INSERT INTO owners (name, age)
+VALUES ('Yuki', '67');
+INSERT INTO owners (name, age)
+VALUES ('Erin', '21');
+INSERT INTO owners (name, age)
+VALUES ('Siobhan', '55');
+INSERT INTO owners (name, age)
+VALUES ('Ronald', '25');
+INSERT INTO owners (name, age)
+VALUES ('Marla', '32');
+INSERT INTO owners (name, age)
+VALUES ('Imogen', '46');
 
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Archstone', '20', '1');
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Zenith Hills', '10', '2');
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Willowspring', '30', '3');
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Ridgefield Bay', '5', '4');
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Brookvista', '20', '5');
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Goldendale', '15', '6');
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Green Haven', '40', '7');
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Fair Creek', '35', '8');
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Parkview Pointe', '50', '9');
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Royal Gardens Court', '45', '4');
+INSERT INTO properties (name, units, owner_id)
+VALUES ('The Villa', '100', '5');
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Peaceful Living Apts', '5', '9');
+INSERT INTO properties (name, units, owner_id)
+VALUES ('Goose Hollow Homes', '10', '6');
